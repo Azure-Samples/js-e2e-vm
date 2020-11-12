fb85d29cff275d0d82c577283ce4a280fee7641b
diberry <diberry@microsoft.com>
2020-11-12 05:54:29 -0800

    edits

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const os = require('os');
 app.get('/', function (req, res) {
     const clientIP = req.headers['x-forwarded-for'];
-    res.send(`Hello World from host ' + os.hostname() + ', ${clientIP}!`)
+    res.send(`Hello World from host ${os.hostname()} ${clientIP}!`)
 })
 app.listen(3000, function () {
     console.log('Hello world app listening on port 3000!')
