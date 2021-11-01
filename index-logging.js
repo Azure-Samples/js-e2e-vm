2e1da3ea5448b586fd41a76e262479556685fadf
Dina Berry <41597107+diberry@users.noreply.github.com>
2021-11-01 12:38:42 -0700

    Update index-logging.js

--- index-logging.js
+++ index-logging.js
@@ -9,7 +9,7 @@ let appInsights = require('applicationinsights');
 
 if(process.env.APPINSIGHTS_INSTRUMENTATIONKEY){
 
-    console.log(`process.env.APPINSIGHTS_INSTRUMENTATIONKEY is found`);
+    console.log(`process.env.APPINSIGHTS_INSTRUMENTATIONKEY is found ${Date.now()`);
     
     appInsights.setup(process.env.APPINSIGHTS_INSTRUMENTATIONKEY)
     .setAutoDependencyCorrelation(true)
@@ -25,25 +25,25 @@ if(process.env.APPINSIGHTS_INSTRUMENTATIONKEY){
     
     client = appInsights.defaultClient;
 } else {
-    console.log(`process.env.APPINSIGHTS_INSTRUMENTATIONKEY not found`);
+    console.log(`process.env.APPINSIGHTS_INSTRUMENTATIONKEY not found ${Date.now()`);
 }
     
 app.get('/trace', (req, res) => {
 
     const clientIP = req.headers['x-forwarded-for'];
-    console.log(`testing from trace route ${clientIP}`)
+    console.log(`testing from trace route ${clientIP} ${Date.now()`)
     
     if(client){
         
         console.log(`client not found`);
         
         client.trackPageView();
-        client.trackTrace({ message: `testing from trace route ${clientIP}`})
+        client.trackTrace({ message: `testing from trace route ${clientIP} ${Date.now()`})
         client.flush();
         
     } else {
         
-        console.log(`client not found`);
+        console.log(`client not found ${Date.now()`);
     }
     
     res.send('tracing...' + clientIP)
@@ -51,8 +51,8 @@ app.get('/trace', (req, res) => {
 
 app.get('/', function (req, res) {
     const clientIP = req.headers['x-forwarded-for'];
-    res.send(`Hello World from host ` + os.hostname() + `, ${clientIP}!`)
+    res.send(`Hello World from host ` + os.hostname() + `, ${clientIP}! ${Date.now()`)
 })
 app.listen(3000, function () {
-    console.log('Hello world app listening on port 3000!')
+    console.log(`Hello world app listening on port 3000! ${Date.now()`)
 })
