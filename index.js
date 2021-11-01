e9517d82951523dc57bb4ea4adfad8540f7ae560
Dina Berry <41597107+diberry@users.noreply.github.com>
2021-11-01 12:35:44 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const os = require('os');
 app.get('/', function (req, res) {
+    console.log(`root ${Date.now()}`)
     const clientIP = req.headers['x-forwarded-for'];
     res.send(`Hello World from host ${os.hostname()} ${clientIP} ${Date.now()}!`)
 })
