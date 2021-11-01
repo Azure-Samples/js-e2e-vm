daa5539f43bcdb487fb2967c09226d23a28d7629
Dina Berry <41597107+diberry@users.noreply.github.com>
2021-11-01 14:08:04 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+const os = require('os');
 const express = require('express')
 const app = express()
-const os = require('os');
+
 app.get('/', function (req, res) {
-    console.log(`root ${Date.now()}`)
+
     const clientIP = req.headers['x-forwarded-for'];
-    res.send(`Hello World from host ${os.hostname()} ${clientIP} ${Date.now()}!`)
+    const msg = `root route ${os.hostname()} ${clientIP} ${new Date()}`
+    console.log(msg)
+
+    res.send(msg)
 })
 app.listen(3000, function () {
     console.log(`Hello world app listening on port 3000! ${Date.now()}`)
