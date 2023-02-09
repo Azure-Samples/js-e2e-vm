3107e77993f4ea195d8d3d0680eb88d99a43122c
Dina Berry (SHE/HER) <Dina.Berry@microsoft.com>
2023-02-09 11:16:30 -0800

    error messages

--- index-logging.js
+++ index-logging.js
@@ -6,32 +6,42 @@ console.log(JSON.stringify(process.env));
 
 const AppInsights = require('applicationinsights');
 
+if (process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
+    console.log(`AppInsights configured with key ${process.env.APPINSIGHTS_INSTRUMENTATIONKEY}`);
+} else{
+    console.log(`AppInsights not configured`);
+}
+
 AppInsights.setup(process.env.APPINSIGHTS_INSTRUMENTATIONKEY)
-.setAutoDependencyCorrelation(true)
-.setAutoCollectRequests(true)
-.setAutoCollectPerformance(true, true)
-.setAutoCollectExceptions(true)
-.setAutoCollectDependencies(true)
-.setAutoCollectConsole(true)
-.setUseDiskRetryCaching(true)
-.setSendLiveMetrics(false)
-.setDistributedTracingMode(AppInsights.DistributedTracingModes.AI)
-.start();
+    .setAutoDependencyCorrelation(true)
+    .setAutoCollectRequests(true)
+    .setAutoCollectPerformance(true, true)
+    .setAutoCollectExceptions(true)
+    .setAutoCollectDependencies(true)
+    .setAutoCollectConsole(true)
+    .setUseDiskRetryCaching(true)
+    .setSendLiveMetrics(false)
+    .setDistributedTracingMode(AppInsights.DistributedTracingModes.AI)
+    .start();
 
 const AppInsightsClient = AppInsights.defaultClient;
 
-   
+
 app.get('/trace', (req, res) => {
 
     const clientIP = req.headers['x-forwarded-for'];
-    const msg = `trace route ${os.hostname()} ${clientIP} ${new Date()} with key ${process.env.APPINSIGHTS_INSTRUMENTATIONKEY}`;
+    const msg = `trace route ${os.hostname()} ${clientIP} ${new Date()}`;
 
     console.log(msg)
 
-    AppInsightsClient.trackPageView();
-    AppInsightsClient.trackTrace({ message: msg})
-    AppInsightsClient.flush();
-    
+    if (process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
+        AppInsightsClient.trackPageView();
+        AppInsightsClient.trackTrace({ message: msg })
+        AppInsightsClient.flush();
+    } else {
+        msg += ' AppInsights not configured';
+    }
+
     res.send(`${msg}`)
 })
 
